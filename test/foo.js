129c1916ffacfcaa3883fd813ab8598a10be8386
frankgreco <frankgreco@northwesternmutual.com>
2017-03-24 17:14:58 -0500

    changes

--- test/foo.js
+++ test/foo.js
@@ -1,4 +1,7 @@
 var assert = require('assert');
+var helpers = require('yeoman-test');
+var path = require('pathy')
+
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
